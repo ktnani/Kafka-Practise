7dc46d1fc4ea46f7932a25af1fcf7e6c5ef31b9b
koushik <koushik.t29@gmail.com>
2025-04-10 21:00:51 -0400

    Add shell script for generation of valid and invalid topic

---
+++ Create_topics/invalid-pos-topic.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export KAFKA_HOME=/Users/koushikreddy/Downloads/confluent-7.9.0
+$KAFKA_HOME/bin/kafka-topics --create --bootstrap-server localhost:9092 --topic invalid-pos-topic --partitions 5 --replication-factor 3 --config segment.bytes=1000000
